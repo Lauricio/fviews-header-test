2e20285b7f86d590aa7a4bb451a556b804d333eb
Lauricio Su <me@lauricio.com>
2014-11-16 16:13:31 +0700

    replaced router with dynamic template

--- header-test.js
+++ header-test.js
@@ -8,10 +8,21 @@ Router.route('/', function () {
 
 
 Router.route('/route1', function () {
+  Session.set("activeHeader","route1header");
   this.render('route1');
 });
 
 Router.route('/route2', function () {
+  Session.set("activeHeader","route2header");
   this.render('route2');
 });
+
+if (Meteor.isClient) {
+  Session.setDefault("activeHeader","route1header");
+  Template.layout.helpers({
+    activeHeader : function () {
+     return Session.get("activeHeader");
+    }
+  });
+};
 
